2bc08b0164aed54fc67d0f3d6e205022db7b0b47
Faith <fisougi0@email.cpcc.edu>
2025-02-16 17:12:42 -0500

    Revert "Update HelloWorld.java"
    This reverts commit d8ddee0a411410c708e8609689f3d3807b30329a.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,18 +2,6 @@ public class HelloWorld
 {
    public static void main(Strings[] args)
    {
-      Systems.out.printLn("Hello World!");
-      
-      //name is a reference variable
-      String name = "George Washington";
-      
-      Systems.out.println(name);
-      
-      //age is a primitive variable
-      int age = 24;
-      
-      Systems.out.printLn(age);
-      
-      
+      Systems.out.PrintLn("Hello World!");
    }
  }
