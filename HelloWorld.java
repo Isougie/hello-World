d8ddee0a411410c708e8609689f3d3807b30329a
Faith <fisougi0@email.cpcc.edu>
2025-02-16 17:11:31 -0500

    Update HelloWorld.java
    Additional print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,18 @@ public class HelloWorld
 {
    public static void main(Strings[] args)
    {
-      Systems.out.PrintLn("Hello World!");
+      Systems.out.printLn("Hello World!");
+      
+      //name is a reference variable
+      String name = "George Washington";
+      
+      Systems.out.println(name);
+      
+      //age is a primitive variable
+      int age = 24;
+      
+      Systems.out.printLn(age);
+      
+      
    }
  }
